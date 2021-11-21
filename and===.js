e0bf319ea50ab5afd3fd4e246327a37155b8dfc1
prashant123yadav <py33680@gmail.com>
2021-11-22 00:10:32 +0530

    boolean null

---
+++ and===.js
@@ -0,0 +1,4 @@
+var ageofbill=10
+var ageofsally="10"
+console.log(ageofbill==ageofsally)
+console.log(ageofbill!=ageofsally)
---
+++ boolean.js
@@ -0,0 +1,6 @@
+var raju="oneplus"
+var sandeep="oneplus"
+var sonu="redmi"
+console.log(raju===sandeep)
+console.log(raju!==sonu)
+console.log(raju===sonu)

9cde712ae546802fdbea84cfe7bfeba113194527
prashant123yadav <py33680@gmail.com>
2021-11-25 00:18:45 +0530

    loops

---
+++ for.js
@@ -0,0 +1,3 @@
+for(var i=200;i<0;i++){
+    console.log(i)
+}
---
+++ while.js
@@ -0,0 +1,10 @@
+var num=1;
+
+while(num<=10){
+    if(num!==10){
+        console.log("The number is",num,"less then 10");
+    }else {
+        console.log("The number is",num,"the loops is now over");
+    }
+    num++;
+}

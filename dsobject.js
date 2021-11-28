32db1a3249b912e8f5d4a8c570c5ea77448ee022
prashant123yadav <py33680@gmail.com>
2021-11-28 19:02:07 +0530

    dsobj

---
+++ dsobject.js
@@ -0,0 +1,18 @@
+var student={
+    name:"sonu",
+    age :20,
+    grades:{
+        js:25,
+        web:85,
+        java:96
+    }
+}
+console.log(student.name);
+console.log(student.age);
+console.log(student.grades);
+student.gender="male";
+console.log(student.gender);
+delete student.gender;
+console.log(student.gender)
+console.log(student.grades.js)
+    
---
+++ nestedarrexam.js
@@ -0,0 +1,13 @@
+var number = [
+    [1,2,3,4],
+    [5,6,7],
+    [8,9,10,11,12] ];
+    let sum = 0;
+    for(i=0; i<number.lenght ; i++){
+        sum+=[i]
+    }
+    console.log(sum);
+    for(j=0; j<number[i].lenght;j++){
+    console.log(number[i][j]);
+    }
+

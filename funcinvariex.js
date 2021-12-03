d1485b9187e930ad41fdfa0bc831816e8356fb9c
prashant123yadav <py33680@gmail.com>
2021-12-03 23:30:18 +0530

    funinvari

---
+++ funcinvariex.js
@@ -0,0 +1,4 @@
+var divide= function(x,y){return x/y};
+var oprations = [divide];
+for (i=0 ; i<oprations.length;i++)
+{console.log(oprations[i](10,5))}
---
+++ funinvari.js
@@ -0,0 +1,6 @@
+var sum = function(x,y){return x+y};
+var subtract = function(x,y){return x-y};
+var multiply = function(x,y){return x*y};
+var divide = function(x,y){return x/y};
+var oprations  =[sum,subtract,multiply,divide];
+for (var i=0 ; i < oprations.length ; i++){console.log(oprations[i](5,10))} 

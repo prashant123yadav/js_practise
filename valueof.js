27ab6a50e690e6eb342ea1bfe4a7cbb209e8d603
prashant123yadav <py33680@gmail.com>
2021-12-10 23:17:10 +0530

    prototype

---
+++ valueof.js
@@ -0,0 +1,8 @@
+var obj1 = new Object(12.5);
+console.log(obj1.valueOf())
+var obj2 = new Object(null);
+console.log(obj2.valueOf());
+var obj3 = new Object()
+console.log(obj3.valueOf())
+var obj = new Object;
+console.log(obj.valueOf())
